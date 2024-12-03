4057ed02898c4e03109857dd4a436c86f7c7cbce
Derek Konigsberg <dkonigsberg@dektronics.com>
2024-12-02 16:19:53 -0800

    Update panelization configuration for boards
    Tweaked sizes to get tooling holes on a 10mm grid
    Added rail tabs to the transmission boards due to corner features
    Removed web generation because pcbdraw is broken

--- hardware/tools/kikit-post.py
+++ hardware/tools/kikit-post.py
@@ -2,6 +2,7 @@ from kikit.units import mm
 from kikit.common import resolveAnchor
 from kikit.defs import EDA_TEXT_HJUSTIFY_T, EDA_TEXT_VJUSTIFY_T, Layer
 import pcbnew
+import sys
 
 def kikitPostprocess(panel, arg):
     # Add revision information to bottom rail
@@ -14,20 +15,28 @@ def kikitPostprocess(panel, arg):
         if isinstance(element, pcbnew.PCB_DIMENSION_BASE) and element.GetLayerName() == "User.Comments":
             panel.board.Remove(element)
 
+    # Prepare panel dimension anchors
+    minx, miny, maxx, maxy = panel.panelBBox()
+    tlAnchor = pcbnew.VECTOR2I(int(minx), int(miny))
+    trAnchor = pcbnew.VECTOR2I(int(maxx), int(miny))
+    blAnchor = pcbnew.VECTOR2I(int(minx), int(maxy))
+    
     # Add top panel dimension
     dim = pcbnew.PCB_DIM_ALIGNED(panel.board)
-    dim.SetStart(resolveAnchor("tl")(panel.boardSubstrate.boundingBox()))
-    dim.SetEnd(resolveAnchor("tr")(panel.boardSubstrate.boundingBox()))
+    dim.SetStart(tlAnchor)
+    dim.SetEnd(trAnchor)
     dim.SetHeight(int(-2.5 * mm));
     dim.SetUnits(pcbnew.EDA_UNITS_MILLIMETRES)
+    dim.SetPrecision(pcbnew.DIM_PRECISION_X_X)
     dim.SetLayer(Layer.Cmts_User)
     panel.board.Add(dim)
     
     # Add side panel dimension
     dim = pcbnew.PCB_DIM_ALIGNED(panel.board)
-    dim.SetStart(resolveAnchor("tl")(panel.boardSubstrate.boundingBox()))
-    dim.SetEnd(resolveAnchor("bl")(panel.boardSubstrate.boundingBox()))
+    dim.SetStart(tlAnchor)
+    dim.SetEnd(blAnchor)
     dim.SetHeight(int(2.5 * mm));
     dim.SetUnits(pcbnew.EDA_UNITS_MILLIMETRES)
+    dim.SetPrecision(pcbnew.DIM_PRECISION_X_X)
     dim.SetLayer(Layer.Cmts_User)
     panel.board.Add(dim)
